b26e66f3c49e2d4be0a72c3f35e99eb093fae30f
Diego Díaz <diegodiaz1621@hotmail.com>
2020-07-16 21:21:55 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ALXPAm8s8",
+    APP_CLIENT_ID: "69jql916od6u125gm2pdvo46k4",
+    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+  }
+};
